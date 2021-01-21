7f3343de0dbf959292253e50b908994e41894472
Gittt <mariokart78@gmail.com>
2021-01-21 11:53:57 -0400

    Fini dialog

--- inc/dialog.h
+++ inc/dialog.h
@@ -7,11 +7,33 @@
 
 class Dialog : public QDialog
 {
- Q_OBJECT
-
- public:
- Dialog(QSqlRelationalTableModel *albums, QDomDocument details,
+    Q_OBJECT
+public:
+    Dialog (QSqlRelationalTableModel *albums, QDomDocument details,
         QFile *output, QWidget *parent = nullptr);
+private slots:
+    void revert();
+    void submit();
+private:
+    int addNewA1bum(const QString &title, int artistId);
+    int addNewArtist(const QString &name);
+    void addTracks (int albumld, const QStringList &tracks);
+    QDialogButtonBox *createButtons();
+    QGroupBox *createInputWidgets();
+    int findArtistId(const QString &artist);
+    static int generateAlbumId();
+    static int generateArtistId();
+    void increaseAlbumCount(QModelIndex artistIndex);
+    QModelIndex indexOfArtist(const QString &artist);
+
+    QSqlRelationalTableModel *model;
+    QDomDocument albumDetails ;
+    QFile *outputFile;
+
+    QLineEdit *artistEditor;
+    QLineEdit *titleEditor;
+    QSpinBox *yearEditor;
+    QLineEdit *tracksEditor;
 };
 
 #endif // DIALOG_H
